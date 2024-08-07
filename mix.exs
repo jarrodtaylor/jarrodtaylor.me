8d8096a77762f1ededc30bb4c3d3b7411947b6b2
Jarrod Taylor <jarrod@mac-mini.local>
2024-08-07 14:49:32 -0400

    Removes test environment

--- mix.exs
+++ mix.exs
@@ -23,8 +23,6 @@ defmodule HyperText.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
@@ -37,7 +35,6 @@ defmodule HyperText.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
       {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
-      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 1.0"},
