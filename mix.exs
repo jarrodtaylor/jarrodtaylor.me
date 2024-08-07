8f0e28baa7c2401bcfa7f3ad0162c6ade490de6f
Jarrod Taylor <jarrod@mac-mini.local>
2024-08-07 14:53:07 -0400

    Adds packages for markdown and yaml

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HyperText.MixProject do
   def project do
     [
       app: :hyper_text,
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -13,9 +13,6 @@ defmodule HyperText.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {HyperText.Application, []},
@@ -25,11 +22,9 @@ defmodule HyperText.MixProject do
 
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # Phoenix packages:
       {:phoenix, "~> 1.7.14"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
@@ -41,16 +36,14 @@ defmodule HyperText.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+
+      # Project packages:
+      {:earmark, "~> 1.4"},
+      {:yamerl, "~> 0.10.0"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "assets.setup", "assets.build"],
