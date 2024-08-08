f54593fa606dfa748814a79daa0d91152a84051e
Jarrod Taylor <jarrod@mac-mini.local>
2024-08-07 22:21:20 -0400

    Adds /weblog/* skeleton content

--- lib/hyper_text_web/controllers/weblog/html.ex
+++ lib/hyper_text_web/controllers/weblog/html.ex
@@ -1,4 +1,27 @@
 defmodule HyperTextWeb.WeblogHTML do
 	use HyperTextWeb, :html
-	embed_templates "templates/*"
+	
+	def recents(assigns) do
+		~H"""
+		Recents
+		"""
+	end
+	
+	def index(assigns) do
+		~H"""
+		Index
+		"""
+	end
+	
+	def archive(assigns) do
+		~H"""
+		Archive
+		"""
+	end
+	
+	def slug(assigns) do
+		~H"""
+		Slug
+		"""
+	end
 end
